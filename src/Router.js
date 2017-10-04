6b7619e1becd2d6daf3d157c5f7aadce30d73935
brain <nvtam2105@gmail.com>
2017-10-04 13:52:32 +0700

    Add drawer content

--- src/Router.js
+++ src/Router.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import { Scene, Router, Actions, DefaultRenderer, Stack } from 'react-native-router-flux';
+import { Scene, Router, Actions, Drawer, Stack } from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import TalkList from './components/TalkList';
 import TalkDetail from './components/TalkDetail';
 import TalkVideo from './components/TalkVideo';
 import TalkScript from './components/TalkScript';
+import DrawerContent from './components/DrawerContent';
 
-import SideMenu from 'react-native-side-menu';
-import DrawerNavigation from './components/DrawerNavigation';
-import NavigationDrawer from './components/NavigationDrawer';
-
-
-
+const menuIcon = (<Icon name="bars" size={25}/>);
 
 const RouterComponent = () => {
   return (
       <Router>
-            <Scene key='drawer' 
-                onEnter={() => {console.log('Enter')}}
-                drawerImage={require('../assets/images/menu_ic.png')} component={NavigationDrawer}
-                open={false}>
-                <Scene key="talkList"  component={TalkList} title="TED Talks" initial />
-                <Scene key="talkDetail" component={TalkDetail} backTitle=" " hideNavBar={false}/>
-                <Scene key="talkVideo"  component={TalkVideo} backTitle=" "/>
-                <Scene key="talkScript" component={TalkScript} backTitle=" "/>
-            </Scene>
+        <Scene key="root">
+            <Drawer hideNavBar key="drawer" contentComponent={DrawerContent} drawerIcon={menuIcon}>
+                <Scene key="talkList" component={TalkList} title="TED Talks" initial />
+            </Drawer>
+            <Scene key="talkDetail" component={TalkDetail} backTitle=" "/>
+            <Scene key="talkVideo"  component={TalkVideo} backTitle=" "/>
+            <Scene key="talkScript" component={TalkScript} backTitle=" "/>
+        </Scene>
       </Router>
   );
 };
---
+++ src/components/DrawerContent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, View, ViewPropTypes } from 'react-native';
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+});
+
+class DrawerContent extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    sceneStyle: ViewPropTypes.style,
+    title: PropTypes.string,
+  }
+
+  static contextTypes = {
+    drawer: React.PropTypes.object,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Drawer Content</Text>
+        <Button onPress={Actions.talkList}>Back</Button>
+      </View >
+    );
+  }
+}
+
+export default DrawerContent;
