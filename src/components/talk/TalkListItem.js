7f2a4abbc6283b09ff16e73b2eb9b4e5c9c84d22
tam <tam@Tams-MacBook-Pro.local>
2017-10-21 01:56:58 +0700

    update gui talk list and detail

--- src/components/talk/TalkListItem.js
+++ src/components/talk/TalkListItem.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { CardSection, Thumbnail } from '../common';
-import { View, Row, Caption, Image, Text, Subtitle, Tile, Title, Overlay, Icon } from '@shoutem/ui';
-import moment  from 'moment';
+import { View, Row, Caption, Image, Subtitle } from '@shoutem/ui';
+import moment from 'moment';
 
 class TalkListItem extends Component {
 
     onRowPress() {
         Actions.talkDetail({ talk: this.props.talk });
     }
-
-
     render() {
         const { talk } = this.props;
         return (
@@ -24,11 +21,7 @@ class TalkListItem extends Component {
                         <Subtitle>{talk.name}</Subtitle>
                         <View styleName="horizontal space-between">
                             <Caption>{moment(talk.published_at).fromNow()}</Caption>
-                            {/* <Caption>{talk.speaker}</Caption> */}
-                            
-                            <Caption>{moment.utc(talk.length).format("mm[m]")}</Caption>
-                            {/* <Caption>#{talk.tag}</Caption> */}
-                            {/* <Caption>{talk.viewed_count}K</Caption> */}
+                            <Caption>{talk.length > 0 && (moment.utc(talk.length).format("mm[m]"))}</Caption>
                         </View>
                     </View>
                 </Row>
@@ -36,15 +29,5 @@ class TalkListItem extends Component {
         );
     }
 }
-
-const styles = {
-    titleStyle: {
-        flex: 1,
-        fontSize: 18,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingLeft: 15
-    }
-};
 
 export default TalkListItem;
--- src/components/talk/detail/TalkDetail.js
+++ src/components/talk/detail/TalkDetail.js
@@ -1,22 +1,15 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
-
 import { Actions } from 'react-native-router-flux';
-import RNFetchBlob from 'react-native-fetch-blob';
-import ProgressBar from 'react-native-progress/Bar';
-
 import { connect } from 'react-redux';
 import { scriptFetch } from '../../../actions';
 import store from '../../../stores';
+import moment from 'moment';
 
-import { Image, ImageBackground, TouchableWithoutFeedback, ActivityIndicator } from "react-native";
+import RNFetchBlob from 'react-native-fetch-blob';
+import ProgressBar from 'react-native-progress/Bar';
+import { TouchableWithoutFeedback, ActivityIndicator } from "react-native";
 import { CardSection, Thumbnail, VideoPlayer } from '../../common';
-
-
-
-
-
-import { View, Row, Caption, Text, Subtitle, Tile, Title, Overlay, Icon, Button } from '@shoutem/ui';
+import { ScrollView, Screen, Image, Divider, View, Row, Caption, Text, Subtitle, Tile, Title, Overlay, Icon, Button } from '@shoutem/ui';
 
 class TalkDetail extends Component {
 
@@ -42,8 +35,8 @@ class TalkDetail extends Component {
     onPressPratice() {
         this.setState({ loading: true })
 
-         // // Download Video
-         RNFetchBlob.config({
+        // // Download Video
+        RNFetchBlob.config({
             fileCache: true,
             appendExt: 'mp4'
         }).fetch('GET', this.props.talk.media, {
@@ -73,7 +66,7 @@ class TalkDetail extends Component {
         //     console.log('The file image saved to ', res.path());
         //     this.props.talk.image = res.path();
         //     store.saveTalk(this.props.talk, this.props.script);
-           
+
         // }).catch((err) => {
         //     console.log(err);
         // });
@@ -94,74 +87,87 @@ class TalkDetail extends Component {
     }
 
     render() {
-        const { stretch } = styles;
         const { talk } = this.props;
         return (
-            <View style={{ flex: 1 }}>
-                <ImageBackground style={stretch} source={{ uri: talk.image }} >
-                    <TouchableWithoutFeedback onPress={this.onPressPlay.bind(this)}>
-                        <Image style={stretch} source={require('../../../../assets/img/play_bt.png')} />
-                    </TouchableWithoutFeedback>
-                </ImageBackground>
+            <Screen>
+                <Image
+                    //styleName="large-portrait hero"
+                    //animationName="hero"
+                    styleName="large-banner hero"
+                    source={{ uri: talk.image }}>
+                    <Tile styleName="sm-gutter-horizontal">
+                        <Overlay styleName="rounded-small">
+                            <Icon name="play" />
+                        </Overlay>
+                        {/* <Tile animationName="hero"> */}
+                        <Subtitle>Content courtesy of TED.com</Subtitle>
+                        {/* </Tile> */}
+                    </Tile>
+                </Image>
+                <ScrollView>
+                    <Screen styleName="paper">
+                        <Subtitle styleName="md-gutter multiline">{talk.name}</Subtitle>
+                        <Divider styleName="line" />
+                        <Row>
+                            <Caption>{moment(talk.published_at).fromNow()}</Caption>
+                            <Caption>{talk.length > 0 && (moment.utc(talk.length).format("mm[m]"))}</Caption>
+                            <Caption>{talk.speaker}</Caption>
+                            <Caption>#{talk.tag}</Caption>
+                        </Row>
+                        <Divider styleName="line" />
+                        <View styleName="horizontal">
+                            {!this.props.script && (
+                                <Button styleName="full-width">
+                                    <Text>NO DICTATION AVALABLE</Text>
+                                </Button>
+                            )
+                            }
+                            {!this.state.loading && this.props.script && (
+                                <Button styleName="dark full-width"
+                                    onPress={this.onPressPratice.bind(this)}
+                                    accessibilityLabel="Learn more about this purple button">
+                                    <Text>PRACTICE</Text>
+                                </Button>
+                            )
+                            }
+                            {this.state.loading && this.props.script && (
+                                <View>
+                                    <ProgressBar
+                                        width={150}
+                                        progress={this.state.progress}
+                                    />
+                                    <ActivityIndicator style={{ flex: 1 }}
+                                        animating={this.state.loading}
+                                        size="small" />
+                                </View>)
+                            }
 
-                <Text>{talk.description}</Text>
 
+                            <Button styleName="dark full-width"
+                                onPress={this.onPressFillGap.bind(this)}>
+                                <Text>FILL GAP</Text>
+                            </Button>
+                            <Button styleName="dark full-width"
+                                onPress={this.onPressPlay.bind(this)}>
+                                <Text>PLAY</Text>
+                            </Button>
 
-                {!this.props.script && (
-                    <Button styleName="dark">
-                        <Text>NO DICTATION AVALABLE</Text>
-                    </Button>
-                )
-                }
-                {!this.state.loading && this.props.script && (
-                    <Button styleName="dark"
-                        onPress={this.onPressPratice.bind(this)}
-                        accessibilityLabel="Learn more about this purple button">
-                        <Text>PRACTICE</Text>
-                    </Button>
-                )
-                }
-                {this.state.loading && this.props.script && (
-                    <View>
-                        <ProgressBar
-                            width={150}
-                            progress={this.state.progress}
-                        />
-                        <ActivityIndicator style={{ flex: 1 }}
-                            animating={this.state.loading}
-                            size="small" />
-                    </View>)
-                }
+                        </View>
+                        <View styleName="horizontal">
+                            <Button styleName="dark full-width"
+                                onPress={this.onPressScript.bind(this)}>
+                                <Text>SCRIPT</Text>
+                            </Button>
+                        </View>
+                        <Divider styleName="line" />
+                        <Text styleName="md-gutter multiline">{talk.description}</Text>
+                    </Screen>
+                </ScrollView>
+            </Screen>
 
-
-
-
-                <Button styleName="dark"
-                    onPress={this.onPressFillGap.bind(this)}>
-                    <Text>FILL GAP</Text>
-                </Button>
-
-                <Button styleName="dark"
-                    onPress={this.onPressPlay.bind(this)}>
-                    <Text>PLAY</Text>
-                </Button>
-
-                <Button styleName="dark"
-                    onPress={this.onPressScript.bind(this)}>
-                    <Text>SCRIPT</Text>
-                </Button>
-            </View>
         );
     }
 }
-
-const styles = {
-    stretch: {
-        width: 100,
-        height: 100
-    }
-};
-
 
 const mapStateToProps = state => {
     return { script: state.script };
