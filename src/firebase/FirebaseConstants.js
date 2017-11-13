21756bafb923c1eb53d93435d437a77bde10675c
brain <nvtam2105@gmail.com>
2017-11-13 15:22:06 +0700

    testing remote fcm

--- src/firebase/FirebaseConstants.js
+++ src/firebase/FirebaseConstants.js
@@ -1,6 +1,6 @@
 
 const FirebaseConstants = {
-	KEY: "AIzaSyCf2y0nW56CTkzm9Fs8OdqDGpoVHwBSBFA",
+	KEY: "AAAA3VUBBPo:APA91bFw5XvkBD6vpzDutMaj8zL7T2oldW-p8feQ_85v5IXLPENbYtBVT9lEAT6CuzUqXb6fGsIxhbcoCYPo83NsCng7YhG9kMbdsCgwogNpldRQr7Q-Xi6e00tLD0Eo1b4VUdcHsPCi",
 	KEY_ANDROID: "AIzaSyDef1MmEpdd2zRY4XIwUXg7b9zzSsC4kKY"
 }
 
